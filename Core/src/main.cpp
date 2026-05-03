7c0980285ae44b76849e0a5f1e39370f0cf18597
Hakim Zekri <62521050+HZekriDEV@users.noreply.github.com>
2026-05-03 17:27:30 -0400

    Vulkan version initial setup

---
+++ Core/src/main.cpp
@@ -0,0 +1,11 @@
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello worlds" << std::endl;
+
+    return 0;
+}
--- shaders/light_fragment.frag
+++
@@ -1,8 +0,0 @@
-#version 330 core
-
-out vec4 PixelColor;
-
-void main()
-{
-	PixelColor = vec4(1.0f);
-}	
