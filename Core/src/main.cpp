4844e796a5ef5dc94d2b5828e592b5a5959d65cb
Hakim Zekri <62521050+HZekriDEV@users.noreply.github.com>
2026-05-05 23:29:35 -0400

    Some basic code

--- Core/src/main.cpp
+++ Core/src/main.cpp
@@ -1,11 +1,49 @@
+#if defined(__INTELLISENSE__) || !defined(USE_CPP20_MODULES)
+#include <vulkan/vulkan_raii.hpp>
+#else
+import vulkan_hpp;
+#endif
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+class HelloTriangleApplication {
+public:
+    void run() {
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    void initVulkan() {
+		std::cout << "Initializing Vulkan..." << std::endl; 
+    }
+
+    void mainLoop() {
+		std::cout << "Entering main loop..." << std::endl;
+    }
+
+    void cleanup() {
+		std::cout << "Cleaning up resources..." << std::endl;
+    }
+};
 
 int main()
 {
-    std::cout << "Hello worlds" << std::endl;
+    try
+    {
+        HelloTriangleApplication app;
+        app.run();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
